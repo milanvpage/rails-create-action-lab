4a290357f62f57d0b29f2701cbc45cd1a9786066
Milan <milanvpage@gmail.com>
2021-07-10 10:53:49 -0700

    commit tree 3fdf212de7837b26809ed23f7429764fe8d3b3a7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,22 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @studnt = Student.new
   end
 
   def create
+    #@student = Student.new
+    #@student.title = params[:title]
+    #@student.description = params[:description]
+    #@student.save
+    #redirect_to student_path(@student)
+    
+      @student = Student.create({
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      })
+      redirect_to student_path(@student)
+    
   end
 
 end
